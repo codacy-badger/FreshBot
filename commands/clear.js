8a1426f1f75bf4ab55c33a76bff04f09565cab45
UnlegitHax <35663531+UnlegitHax@users.noreply.github.com>
2018-06-30 21:35:48 +0200

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -2,12 +2,8 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) {
-    message.reply("You don't have Permission to do that!");
-    }
-  if(!args[0]) {
-    message.channel.send("You must write a amount of messages to delete!");
-    }
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You don't have Permission to do that!");
+  if(!args[0]) return message.channel.send("You must write a amount of messages to delete!");
   message.channel.bulkDelete(args[0]).then(() => {
   message.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete(2000));
 });
