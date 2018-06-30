a926191f4a604e951f44004c171bc37d4a909c6f
UnlegitHax <35663531+UnlegitHax@users.noreply.github.com>
2018-06-30 21:37:33 +0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
     .addField("My Ping", bot.ping + "ms")
     .setColor(0x2ecc71);
 
-    message.channel.send(pingembed)
+    message.channel.send(pingembed);
 }
 
 module.exports.help = {
